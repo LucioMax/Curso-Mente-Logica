8465f485cc6a50d2ef2cdfc6492ca4aa1e0db75e
LucioMax <112509453+LucioMax@users.noreply.github.com>
2024-12-20 12:41:56 -0300

    Calculando o troco

---
+++ dia-3_1.py
@@ -0,0 +1,13 @@
+# Calculando o troco
+
+pao = 3.50
+leite = 4.20
+cafe = 2.80
+
+total_compra = pao + leite + cafe
+
+valor_pago = 20
+
+troco = valor_pago - total_compra
+print("Total da compra: R$", total_compra)
+print("Troco a receber : R$", troco)
