dce615f97d63e2ba0f329ac15c2316d99c2e36fe
LucioMax <112509453+LucioMax@users.noreply.github.com>
2024-12-20 11:58:24 -0300

    Exibindo Informações das Variáveis

---
+++ dia-2_2.py
@@ -0,0 +1,12 @@
+# Exibindo Informações das Variáveis"
+
+nome = "Lúcio Max"
+idade = 37
+altura = 1.82
+estudante = True
+
+
+print("Nome: ", nome)
+print("Idade: ", idade)
+print("Altura: ", altura)
+print("Estudante: ", estudante)
